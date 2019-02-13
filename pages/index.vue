ae9fce39abeb018c7d3ca1051373dbc78498fe79
kristof ws <kristof.dsantos@gmail.com>
2019-02-14 00:52:16 +0100

    test

--- pages/index.vue
+++ pages/index.vue
@@ -3,15 +3,7 @@
 		<img src="~assets/img/logo.png" alt="Nuxt.js Logo" class="logo">
 		<h1 class="title">test autocomplete
       {{ ready }}
-      <no-ssr>
-			<vue-google-autocomplete
-				v-if="ready"
-				id="map"
-				class="form-control"
-				placeholder="Start typing"
-				v-on:placechanged="getAddressData"
-			></vue-google-autocomplete>
-      </no-ssr>
+      
 		</h1>
 	</section>
 </template>
@@ -22,13 +14,7 @@ export default {
   mounted () {
     const _this = this
     _this.ready = true
-    /*
-    var promise1 = Promise.resolve(123)
-
-    promise1.then(function (value) {
-      console.log(value)
-      _this.ready = true
-    }) */
+    console.log(this.ready)
   },
   head () {
     return {
