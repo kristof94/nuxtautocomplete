d51699cef6b2d61cbd225ad3111f892b4a448800
kristof ws <kristof.dsantos@gmail.com>
2019-02-14 00:59:55 +0100

    test

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,12 +12,6 @@ module.exports = {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ],
-    script: [
-      {
-        src:
-          'https://maps.googleapis.com/maps/api/js?key=AIzaSyAWmSTK_5vW944i8kUyotoFxJaTRlr61ns&libraries=places'
-      }
     ]
   },
   /*
